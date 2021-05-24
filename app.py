aa6ef1f1c94ad25fa036ac9b08947016ec2eb300
Sophie Echo Solo <admin@sophiesolo.biz>
2021-05-24 12:34:26 -0400

    added plotting functions

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import csv
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 actDict = dict()
 
 def activityParse():
-    with open("apriltomayactivity.csv", newline='') as csvFile:
+    with open("input.csv", newline='') as csvFile:
         csvReader = csv.DictReader(csvFile)
 
         for row in csvReader:
@@ -15,6 +17,16 @@ def activityParse():
                     actDict[key].append(value)
                 else:
                     actDict[key] = value
+        for key in actDict.keys():
+            if key.lower() != "date" and key.lower() != "floors":
+                actDict.update({key:0})
+                for val in actDict[key]:
+                    val=float(val.replace(',',''))
+                actDict.update({key: val})
+            else:
+                for val in actDict["Date"]:
+                    val = datetime.datetime.strptime(val,"%Y-%m-%d")
+                    actDict["Date"] = val
         
         return actDict
 
@@ -24,21 +36,42 @@ def getAvgs(actDict):
 
     for key in actDict.keys():
         total = 0
-        if key.lower() != "date":
-            avgDict.update({key:[]})
+        if key.lower() != "date" and key.lower() != "floors":
+            avgDict.update({key:0})
             for val in actDict[key]:
                 total+=float(val.replace(',',''))
-            avgDict[key].append(round(total/len(actDict[key]),2))
+            avgDict.update({key: round(total/len(actDict[key]),2)})
         else:
             for val in actDict["Date"]:
                 dates = datetime.datetime.strptime(val,"%Y-%m-%d")
                 dateList.append(dates)
 
     print(f"The average values of each field for the time period of {str(dateList[0])} to {str(dateList[-1])} are as follows: {str(avgDict)}" )
+    return avgDict
             
+def writeExcel(avgDict):
+    with open('output.csv', 'w', newline='') as csvfile:
+        fileWriter = csv.writer(csvfile)
+        for key, value in avgDict.items():
+            fileWriter.writerow([key, value])
 
+#test plot
+def plotData(actDict):
+
+    for k, v in actDict.items():
+        plt.title(k.upper())
+        plt.xlabel("Days")
+        plt.ylabel("y")
+        plt.figure(k)
+        xs = range(0,len(v))
+        ys = v
+        plt.plot(xs, ys, '-,', label=k)
+        plt.legend()
+        plt.savefig(f"{k.replace(' ', '-')}-plot.png")
 
 
 if __name__ == "__main__":
     newDict = activityParse()
-    getAvgs(newDict)
+    newAvgs = getAvgs(newDict)
+    writeExcel(newAvgs)
+    plotData(newDict)
