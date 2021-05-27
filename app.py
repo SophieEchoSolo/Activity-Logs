8b0a33f268c72357005d110660fa90cacd39c0da
Echo Solo <admin@sophiesolo.biz>
2021-05-27 15:17:51 -0400

    Now using dataframes

--- app.py
+++ app.py
@@ -6,48 +6,40 @@ import os
 import json
 from functools import reduce
 from fitauth import *
+import pandas as pd
+
+TEST_DATA = {'activities-calories': [{'dateTime': '2021-05-19', 'value': '2361'}, {'dateTime': '2021-05-20', 'value': '3188'}, {'dateTime': '2021-05-21', 'value': '2832'}, {'dateTime': '2021-05-22', 'value': '2767'}, {'dateTime': '2021-05-23', 'value': '2665'}, {'dateTime': '2021-05-24', 'value': '2847'}, {'dateTime': '2021-05-25', 'value': '1920'}],
+             'activities-steps': [{'dateTime': '2021-05-19', 'value': '1148'}, {'dateTime': '2021-05-20', 'value': '7671'}, {'dateTime': '2021-05-21', 'value': '5667'}, {'dateTime': '2021-05-22', 'value': '4629'}, {'dateTime': '2021-05-23', 'value': '4555'}, {'dateTime': '2021-05-24', 'value': '5314'}, {'dateTime': '2021-05-25', 'value': '4237'}]}
 
 
 def activityParse(activities):
-    # print(activities)
-    # Needs to be updated due to fitauth implementation
+    for key, val in activities.items():
+        for index in range(len(val)):
+            for key in val[index]:
+                if key.lower() == "datetime":
+                    newVal = datetime.datetime.strptime(
+                        val[index][key], "%Y-%m-%d").date()
+                    val[index].update({key: newVal})
+                else:
+                    newVal = float(val[index][key].replace(',', ''))
+                    val[index].update({key: newVal})
     return activities
 
 
-def getAvgs(actDict):
-    # Needs to be updated due to fitauth implementation
-    avgDict = {}
-    dateList = []
-
+def getDataframes(actDict):
+    # Need to sort out plots stacking
     for key in actDict.keys():
-        total = 0
-        if key.lower() != "date" and key.lower() != "floors":
-            avgDict.update({key: 0})
-            for val in actDict[key]:
-                print(val)
-                total += val
-            avgDict.update({key: round(total/len(actDict[key]), 2)})
-        else:
-            for val in actDict["Date"]:
-                dateList.append(val)
+        df = pd.DataFrame()
+        df = pd.DataFrame(data=newDict[key])
+        df = df.sort_values('dateTime', ascending=True)
 
-    print(
-        f"The average values of each field for the time period of {str(dateList[0])} to {str(dateList[-1])} are as follows: {str(avgDict)}")
-    return avgDict
-
-
-def plotData(actDict):
-
-    for k, v in actDict.items():
-        plt.title(k.upper())
-        plt.xlabel("Days")
-        plt.ylabel("y")
-        plt.figure(k)
-        xs = range(0, len(v))
-        ys = v
-        plt.plot(xs, ys, '-,', label=k)
-        plt.legend()
-        plt.savefig(f"{k.replace(' ', '-')}-plot.png")
+        plt.title(key.upper())
+        plt.xlabel("Dates")
+        plt.ylabel(key[key.find('-')+1:].capitalize())
+        plt.plot(df['dateTime'], df['value'])
+        plt.xticks(rotation=30)
+        plt.tight_layout()
+        plt.savefig(f"{key}-plot.png")
 
 
 if __name__ == "__main__":
@@ -58,7 +50,10 @@ if __name__ == "__main__":
     auth = loadAuth()
     authd_client = fitbit.Fitbit(KEY, SECRET,
                                  access_token=auth['access_token'], refresh_token=auth["refresh_token"], refresh_cb=saveAuth, expires_at=auth["expires_at"])
-    newDict = activityParse(reduce(mergeDict, map(lambda a: authd_client.time_series(
-        a, period="1d"), actList)))
-    newAvgs = getAvgs(newDict)
-    # plotData(newDict)
+
+    # activityData = reduce(mergeDict, map(lambda a: authd_client.time_series(
+    #     a, period="7d"), actList))
+
+    activityData = TEST_DATA
+    newDict = activityParse(activityData)
+    getDataframes(newDict)
